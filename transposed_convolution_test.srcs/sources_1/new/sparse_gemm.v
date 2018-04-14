2e3a6f56c909a9897161ece308e1c1ba1a00c86b
Yifan Yu <yvifan@gmail.com>
2018-04-14 17:41:28 +0300

    Use DSP48 Macro to compute the signals that have long propagation delays with fabric combinational circuit

--- transposed_convolution_test.srcs/sources_1/new/sparse_gemm.v
+++ transposed_convolution_test.srcs/sources_1/new/sparse_gemm.v
@@ -41,17 +41,18 @@ module sparse_gemm
     output reg done_tick
 );
 
-localparam [1:0]
-    idle = 2'b00,
-    loop = 2'b01,
-    macc = 2'b10,
-    done = 2'b11;
+// TODO: add a state to clear output BRAM
+localparam [2:0]
+    idle = 3'b000,
+    loop = 3'b001,
+    pick = 3'b010,
+    macc = 3'b011,
+    done = 3'b100;
 
-reg [1:0] state, state_next;
+reg [2:0] state, state_next;
 reg [ADDR_WIDTH-1:0] a_rd_addr_reg, a_rd_addr_next;
 reg [ADDR_WIDTH-1:0] b_rd_addr_reg, b_rd_addr_next;
 reg [ADDR_WIDTH-1:0] c_rw_addr_reg, c_rw_addr_next;
-reg [ACC_WIDTH-1:0] sum, sum_next;
 
 reg [MKN_WIDTH-1:0] i, i_next;
 reg [MKN_WIDTH-1:0] j, j_next;
@@ -69,6 +70,16 @@ reg [MISC_WIDTH-1:0] w_offset, w_offset_next;
 reg [INOUT_WH_WIDTH-1:0] h_col, h_col_next;
 reg [INOUT_WH_WIDTH-1:0] w_col, w_col_next;
 
+// DSP48 Macro reference says: "When the specified value (of P port) is less than the
+// full precision width, the output is truncated, that is, the **LSBs** are removed."
+//
+// This is why we need to take the full precision P and truncate it down to INOUT_WH_WIDTH
+wire signed [INOUT_WH_WIDTH-1:0] h_im, w_im;
+wire [47:0] h_im_p, w_im_p, a_rd_addr_p, b_rd_addr_p, c_rw_addr_p;
+wire [47:0] p0, p1, p2, p3, p4;
+wire [47:0] acc;
+reg macc_reset, macc_ce; // reset & clock enable
+
 always @(posedge clk, posedge reset)
 begin
     if (reset)
@@ -77,7 +88,6 @@ begin
             a_rd_addr_reg <= 0;
             b_rd_addr_reg <= 0;
             c_rw_addr_reg <= 0;
-            sum <= 0;
 
             i <= 0;
             j <= 0;
@@ -99,7 +109,6 @@ begin
             a_rd_addr_reg <= a_rd_addr_next;
             b_rd_addr_reg <= b_rd_addr_next;
             c_rw_addr_reg <= c_rw_addr_next;
-            sum <= sum_next;
 
             i <= i_next;
             j <= j_next;
@@ -189,7 +198,6 @@ begin
     a_rd_addr_next = a_rd_addr_reg;
     b_rd_addr_next = b_rd_addr_reg;
     c_rw_addr_next = c_rw_addr_reg;
-    sum_next = sum;
 
     i_next = i;
     j_next = j;
@@ -205,11 +213,14 @@ begin
     w_col_next = w_col;
 
     // direct output signals
+    c_out = 0;
     c_wr_en = 1'b0; // a tick
     done_tick = 1'b0;
+    macc_reset = 1'b0;
+    macc_ce = 1'b0;
 
     case (state)
-        idle:
+        idle: // 0
             begin
                 if (start_tick)
                     begin
@@ -229,7 +240,7 @@ begin
                         last_next = 1'b0;
                     end
             end
-        loop:
+        loop: // 1
             begin
                 if (c_im == n_output_plane - 1)
                     begin
@@ -292,43 +303,52 @@ begin
                             inner_loop3();
                     end
 
-                h_im = h_col * stride_h - pad_h + h_offset * dilation_h;
-                w_im = w_col * stride_w - pad_w + w_offset * dilation_w;
+                state_next = pick;
+                // it is important to save i and j's values here use the saved value for macc
+                i_saved_next = i;
+                j_saved_next = j;
+                a_rd_addr_next = i;
+                b_rd_addr_next = j;
+            end
+        pick: // 2
+            begin
+                // a & b are available in this cycle, DSP48 has 1 register delay (the M register), so acc will be
+                // a * b in the next cycle
                 if (h_im >= 0 && h_im < output_h && w_im >= 0 && w_im < output_w)
                     begin
                         state_next = macc;
-                        a_rd_addr_next = i;
-                        b_rd_addr_next = j;
-                        c_rw_addr_next = (c_im * output_h + h_im) * output_w + w_im;
-                        sum_next = 0;
-                        // it is important to save i and j's values here use the saved value for macc
-                        i_saved_next = i;
-                        j_saved_next = j;
-                        l_next = 0;
+                        // c_rw_addr_p depends on c_im, h_im and w_im, which are computed for the previous loop cycle
+                        c_rw_addr_next = c_rw_addr_p[ADDR_WIDTH-1:0];
+                        l_next = 1; // set to 1 so that in the next cycle, a_rd_addr_next can be computed
+                        macc_ce = 1'b1;
                     end
                 else if (last_next)
                     state_next = done;
+                else
+                    state_next = loop;
             end
-        macc:
+        macc: // 3
             begin
-                sum_next = sum + a * b;
-                if (l == k - 1)
+                if (l == k) // l started with 1 instead of 0
                     begin
                         if (last)
                             state_next = done;
                         else
                             state_next = loop;
-                        c_out = c + sum_next;
+                        c_out = c + acc[ACC_WIDTH-1:0];
                         c_wr_en = 1'b1;
+                        macc_reset = 1'b1;
                     end
                 else
                     begin
                         l_next = l + 1;
-                        a_rd_addr_next = l_next * m + i_saved;
-                        b_rd_addr_next = l_next * n + j_saved;
+                        // these addresses are ready in this cycle since we used l_next in the previous cycle as input to DSP48
+                        a_rd_addr_next = a_rd_addr_p[ADDR_WIDTH-1:0];
+                        b_rd_addr_next = b_rd_addr_p[ADDR_WIDTH-1:0];
+                        macc_ce = 1'b1;
                     end
             end
-        done:
+        done: // 4
             begin
                 state_next = idle;
                 done_tick = 1'b1;
@@ -339,8 +359,126 @@ end
 // The reason that we assign a_rd_addr to a_rd_addr_next instead of a_rd_addr_reg is so that
 // a will be immediately available in the next cycle. Otherwise it's 2 DFF cascaded and a will
 // be delayed a cycle.
+//
+// a_rd_addr_next = l_next * m + i_saved;
+// b_rd_addr_next = l_next * n + j_saved;
+// c_rw_addr = (c_im * output_h + h_im) * output_w + w_im;
 assign a_rd_addr = a_rd_addr_next;
 assign b_rd_addr = b_rd_addr_next;
 assign c_rw_addr = c_rw_addr_next;
+
+// If these were to be implemented in fabric, they pass all simulations but fail on device
+// due to large propagation delays. Therefore, we compute them with DSP48 and wait for a cycle
+// to obtain the results
+//
+// h_im = h_col * stride_h - pad_h + h_offset * dilation_h;
+// w_im = w_col * stride_w - pad_w + w_offset * dilation_w;
+assign h_im = h_im_p[INOUT_WH_WIDTH-1:0]; // explictly truncate down the 48-bit result
+assign w_im = w_im_p[INOUT_WH_WIDTH-1:0];
+
+// h_im = h_col * stride_h - pad_h + h_offset * dilation_h;
+// NOTE: writing 0 instead of 1'b0 will not produce 1-bit width, but the width of int (32)
+// NOTE: since we run the DSP48 slices with our 100MHz clk, there's no need to pipeline
+xbip_dsp48_macro_0 dsp48_h_im_a (
+  .CLK(clk),
+  .SEL(2'd0),
+  .PCIN(48'd0), // PCIN needs to be connected to avoid critical warning
+  .A({15'd0, stride_h}),
+  .B({11'd0, h_col}),
+  .C({29'd0, pad_h}),
+  .PCOUT(p0),
+  .P()
+);
+
+xbip_dsp48_macro_0 dsp48_h_im_b (
+  .CLK(clk),
+  .SEL(2'd1),
+  .PCIN(p0),
+  .A({15'd0, h_offset}),
+  .B({15'd0, dilation_h}),
+  .C(32'd0), // C needs to be connected to avoid critical warning
+  .PCOUT(p1),
+  .P(h_im_p)
+);
+
+// w_im = w_col * stride_w - pad_w + w_offset * dilation_w;
+xbip_dsp48_macro_0 dsp48_w_im_a (
+  .CLK(clk),
+  .SEL(2'd0),
+  .PCIN(48'd0),
+  .A({15'd0, stride_w}),
+  .B({11'd0, w_col}),
+  .C({29'd0, pad_w}),
+  .PCOUT(p2),
+  .P()
+);
+
+xbip_dsp48_macro_0 dsp48_w_im_b (
+  .CLK(clk),
+  .SEL(2'd1),
+  .PCIN(p2),
+  .A({15'd0, w_offset}),
+  .B({15'd0, dilation_w}),
+  .C(32'd0),
+  .PCOUT(p3),
+  .P(w_im_p)
+);
+
+// c_rw_addr_next = (c_im * output_h + h_im) * output_w + w_im;
+xbip_dsp48_macro_0 dsp48_c_rw_addr_a (
+  .CLK(clk),
+  .SEL(2'd1),
+  .PCIN(p1),
+  .A({8'd0, c_im}),
+  .B({11'd0, output_h}),
+  .C(32'd0),
+  .PCOUT(),
+  .P(p4) // PCOUT may only drive PCIN
+);
+
+xbip_dsp48_macro_0 dsp48_c_rw_addr_b (
+  .CLK(clk),
+  .SEL(2'd1),
+  .PCIN(p3),
+  .A(p4[17:0]), // we can't cascade this p4, unfortunately
+  .B({11'd0, output_w}),
+  .C(32'd0),
+  .PCOUT(),
+  .P(c_rw_addr_p)
+);
+
+// sum_next = sum + a * b;
+xbip_dsp48_macro_1 dsp48_macc (
+  .CLK(clk),
+  .CE(macc_ce),
+  .SCLR(macc_reset),
+  .A(a),
+  .B(b),
+  .P(acc)
+);
+
+// a_rd_addr_next = l_next * m + i_saved;
+xbip_dsp48_macro_0 dsp48_a_rd_addr (
+  .CLK(clk),
+  .SEL(2'd2),
+  .PCIN(48'd0),
+  .A({4'd0, l_next}),
+  .B({4'd0, m}),
+  .C({18'd0, i_saved}),
+  .PCOUT(),
+  .P(a_rd_addr_p)
+);
+
+// b_rd_addr_next = l_next * n + j_saved;
+xbip_dsp48_macro_0 dsp48_b_rd_addr (
+  .CLK(clk),
+  .SEL(2'd2),
+  .PCIN(48'd0),
+  .A({4'd0, l_next}),
+  .B({4'd0, n}),
+  .C({18'd0, j_saved}),
+  .PCOUT(),
+  .P(b_rd_addr_p)
+);
 
 endmodule
--- transposed_convolution_test.srcs/sources_1/new/top.v
+++ transposed_convolution_test.srcs/sources_1/new/top.v
@@ -14,7 +14,7 @@ wire we;
 wire [15:0] addr_a, addr_b, addr_c;
 reg [15:0] addr_result = 0;
 wire [7:0] a, b;
-wire [31:0] c, result;
+wire [31:0] c, cw, result;
 wire done_tick;
 reg led_reg = 0;
 wire btn_c_tick, btn_u_tick;
@@ -26,11 +26,14 @@ bram #(.ADDR_WIDTH(16), .DATA_WIDTH(8), .DATA_FILE("weight_1_uint8.data")) bram_
     (.clk(clk), .we(1'b0), .addr_a(16'd0), .addr_b(addr_b), .din_a(8'd0), .dout_a(), .dout_b(b));
 
 bram #(.ADDR_WIDTH(16), .DATA_WIDTH(32)) bram_res
-        (.clk(clk), .we(we), .addr_a(addr_c), .addr_b(addr_result), .din_a(c), .dout_a(), .dout_b(result));
+        (.clk(clk), .we(we), .addr_a(addr_c), .addr_b(addr_result), .din_a(cw), .dout_a(c), .dout_b(result));
 
 sparse_gemm mm_unit
-    (.clk(clk), .reset(1'b0), .m(14'd9), .k(14'd1), .n(14'd4), .start_tick(btn_c_tick),
-     .a(a), .b(b), .a_rd_addr(addr_a), .b_rd_addr(addr_b), .c(c), .c_wr_en(we), .c_wr_addr(addr_c), .done_tick(done_tick));
+    (.clk(clk), .reset(1'b0), .start_tick(btn_c_tick), .m(14'd9), .k(14'd2), .n(14'd4),
+     .n_output_plane(10'd1), .output_h(7'd4), .output_w(7'd4), .input_h(7'd3), .input_w(7'd3),
+     .kernel_h(3'd2), .kernel_w(3'd2), .pad_h(3'd1), .pad_w(3'd1),
+     .stride_h(3'd2), .stride_w(3'd2), .dilation_h(3'd1), .dilation_w(3'd1), .a(a), .b(b), .c(c),
+     .a_rd_addr(addr_a), .b_rd_addr(addr_b), .c_rw_addr(addr_c), .c_out(cw), .c_wr_en(we), .done_tick(done_tick));
 
 disp_hex_mux disp_unit (
     .clk(clk), .reset(1'b0),
